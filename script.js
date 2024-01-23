6f276e8a79ac9e40bf85ffb5094d67b742aa14ac
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-01-23 16:10:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,174 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+
+// document.getElementbyId("btn").addEventListener("click",function(){
+// 	 let num = document.getElementById("ip").value;
+// 	 let output = document.getElementById("output");
+// 	myPromise1(num).then((data)=>{
+// 		console.log(data);
+// 		  output.innerHTML += data.Result;
+// 		  return promise2(data.Result);
+// 	}).then((data)=>{
+// 		console.log(data);
+// 		  output.innerHTML += data.Result;
+// 		  return promise3(data.Result);
+// 	}).then((data)=>{
+// 		console.log(data);
+// 		output.innerHTML += data.Result;
+// 		  return promise4(data.Result);
+// 	}).then((data)=>{
+// 		console.log(data);
+// 		output.innerHTML += data.Result;
+// 		  return promise5(data.Result);
+// 	}).then((data)=>{
+// 		console.log(data);
+// 		output.innerHTML += data.Result;
+// 		  return promise6(data.Result);
+// 	}).then((data)=>{
+// 		output.innerHTML += data.Result;
+// 		console.log(data);
+// 	}).catch((err)=>{
+// 		console.log(err);
+// 	})
+// })
+
+// function myPromise1(num){
+// 	return new Promise((resolve,reject)=>{
+// 		  setTimeout(()=>{
+// 			  resolve({Result : num});
+			  
+// 		  },2000)
+// 	})
+// }
+
+// function promise2(data){
+// 	return new Promise((resolve,reject)=>{
+// 		  setTimeout(()=>{
+// 			  resolve({Result : data});
+			  
+// 		  },2000)
+// 	})
+// }
+
+// function promise3(data){
+// 	return new Promise((resolve,reject)=>{
+// 		  setTimeout(()=>{
+// 			  resolve({Result : data*2});
+			  
+// 		  },1000)
+// 	})
+// }
+
+// function promise4(data){
+// 	return new Promise((resolve,reject)=>{
+// 		  setTimeout(()=>{
+// 			  resolve({Result : data-3});
+			  
+// 		  },1000)
+// 	})
+// }
+
+// function promise5(data){
+// 	return new Promise((resolve,reject)=>{
+// 		  setTimeout(()=>{
+// 			  resolve({Result: data / 2});
+			  
+// 		  },1000)
+// 	})
+// }
+
+// function promise6(data){
+// 	return new Promise((resolve,reject)=>{
+// 		  setTimeout(()=>{
+// 			  resolve({Result : data + 10});
+			  
+// 		  },1000)
+// 	})
+// }
+
+document.getElementById("btn").addEventListener("click", function () {
+    let num = document.getElementById("ip").value;
+    let output = document.getElementById("output");
+
+    myPromise1(num)
+        .then((data) => {
+            console.log(data);
+            output.innerHTML += data.Result+`<br>`;
+            return promise2(data.Result);
+        })
+        .then((data) => {
+            console.log(data);
+            output.innerHTML += data.Result+`<br>`;
+            return promise3(data.Result);
+        })
+        .then((data) => {
+            console.log(data);
+            output.innerHTML += data.Result+`<br>`;
+            return promise4(data.Result);
+        })
+        .then((data) => {
+            console.log(data);
+            output.innerHTML += data.Result+`<br>`;
+            return promise5(data.Result);
+        })
+        .then((data) => {
+            console.log(data);
+            output.innerHTML += data.Result+`<br>`;
+            return promise6(data.Result);
+        })
+        .then((data) => {
+            output.innerHTML += data.Result+`<br>`;
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+function myPromise1(num) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ Result: num });
+        }, 2000);
+    });
+}
+
+function promise2(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ Result: data });
+        }, 2000);
+    });
+}
+
+function promise3(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ Result: data * 2 });
+        }, 1000);
+    });
+}
+
+function promise4(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ Result: data - 3 });
+        }, 1000);
+    });
+}
+
+function promise5(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ Result: data / 2 });
+        }, 1000);
+    });
+}
+
+function promise6(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ Result: data + 10 });
+        }, 1000);
+    });
+}
+
